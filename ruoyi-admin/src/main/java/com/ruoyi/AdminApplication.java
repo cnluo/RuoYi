cec8fe47a2d076304aa7407e72f6a9f19e2f4a4d
luojun <luojun@mmcuav.cn>
2026-01-16 15:43:51 +0800

    x1

---
+++ ruoyi-admin/src/main/java/com/ruoyi/AdminApplication.java
@@ -0,0 +1,16 @@
+package com.ruoyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动程序
+ *
+ * @author ruoyi
+ */
+@SpringBootApplication
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
